aa95cb482a414f307b4249d4723ff8f4dd71d648
Nivetha <nive54@gmail.com>
2022-09-27 23:13:08 +0300

    last

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -2,7 +2,7 @@
 <html dir="rtl" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  @vite('resources/css/app.css')
+  @vite('resources/css/app.css','resources/js/app.js')
 
     <title>Laravel</title>
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
