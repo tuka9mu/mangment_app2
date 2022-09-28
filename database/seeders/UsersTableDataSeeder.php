181d04b664894cfdc08e1d534e67b95dfcbad8dd
Nivetha <nive54@gmail.com>
2022-09-28 22:48:25 +0300

    last import test1

---
+++ database/seeders/UsersTableDataSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UsersTableDataSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            // 'name' => str_random(8),
+            // 'email' => str_random(12).'@mail.com',
+            'name' => 'tuka',
+            'type' => '0',
+            'email' => 'tuka@a.com',
+            'password' => bcrypt('123456')
+        ]);
+    }
+}
